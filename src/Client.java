41ce9f98f7deebef57c2748939af3789ad110e07
FoxFate <solembum.pospolity@gmail.com>
2024-02-12 19:32:15 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 6969);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6969);
+        Socket socket = serverSocket.accept();
+    }
+}
