7e9de93cded0949f9dabf5e9f46564e310007c93
Hubert Jackowski <72974391+pustelnik17@users.noreply.github.com>
2024-03-08 15:28:57 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,6 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 6969);
+        Socket socket = new Socket("localhost", 8080);
     }
 }
